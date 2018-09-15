d3ff3f472aa100773126c63cf36975a7484dea3e
Robin.Zhang <zhangbinatp@gmail.com>
2018-09-15 09:15:15 +0800

    Update FdActivity.java

--- samples/face-detection/src/org/opencv/samples/facedetect/FdActivity.java
+++ samples/face-detection/src/org/opencv/samples/facedetect/FdActivity.java
@@ -143,9 +143,10 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+            //OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
+            System.loadLibrary("opencv_java3");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
     }
